7f519a19620e5202aaac7c9768081181a5fc8195
Charlie Kolb <charlie.kolb@outlook.de>
2017-09-09 22:19:43 +0200

    Started working on basic structure for base full scan

--- CodeAnalyzer.py
+++ CodeAnalyzer.py
@@ -1,34 +1,37 @@
 import os
 from collections import deque, namedtuple
 
+DictData = namedtuple("DictData", "amount_of_files file_name_list dir_path_list")
+
 
 # ToDo handle different input parameters and call the fitting functions
 def main():
     pass
 
-DirData = namedtuple("DirData", "file_count file_names dir_names")
-
 
 def analyze_dir(path):
     files_and_dirs = os.listdir(path)
     file_counter = 0
-    file_names, dir_names = list, list
+    file_names, dir_paths = [], []
     for entry in files_and_dirs:
         if os.path.isfile(entry):
             pass
         else:
             pass
-    return DirData(file_counter, file_names, dir_names)
+    return DictData(file_counter, file_names, dir_paths)
 
 
 def do_the_thing():
-    directory_dict = dict
+    dir_dict = {}
 
     # ToDo add option for user input path
     # deque with absolute path of the file
-    dir_deque = deque(os.path.dirname(os.path.abspath(__file__)))
-    while dir:
-        (file_count, file_list, dir_list) = analyze_dir(dir_deque.popleft())
-        
+    dir_deque = deque()
+    dir_deque.append(os.path.dirname(os.path.abspath(__file__)))
+    while dir_deque:
+        curr_dir = dir_deque.popleft()
+        dir_dict[curr_dir] = analyze_dir(curr_dir)
+        for dir_name in dir_dict[curr_dir].dir_path_list:
+            dir_deque.append(dir_name)
 
 do_the_thing()
