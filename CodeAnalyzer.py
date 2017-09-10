962d5deeef4b4ba279e9f02f937e58f933105027
Charlie Kolb <charlie.kolb@outlook.de>
2017-09-10 09:51:36 +0200

    Program now reads all files and paths in own directory and logs them in a dict

--- CodeAnalyzer.py
+++ CodeAnalyzer.py
@@ -10,28 +10,36 @@ def main():
 
 
 def analyze_dir(path):
+    # print(path)
     files_and_dirs = os.listdir(path)
     file_counter = 0
     file_names, dir_paths = [], []
     for entry in files_and_dirs:
-        if os.path.isfile(entry):
-            pass
+        # print("?: " + path + os.path.sep + entry)
+        if os.path.isfile(path + os.path.sep + entry):
+            # print("file: " + os.path.basename(entry))
+            file_names.append(os.path.basename(entry))
+            file_counter += 1
         else:
-            pass
+            # print("dir: " + path + os.path.sep + entry)
+            dir_paths.append(path + os.path.sep + entry)
     return DictData(file_counter, file_names, dir_paths)
 
 
-def do_the_thing():
+def do_the_thing(start_dir=os.path.dirname(os.path.abspath(__file__))):
     dir_dict = {}
-
+    start_dir = start_dir
     # ToDo add option for user input path
     # deque with absolute path of the file
     dir_deque = deque()
-    dir_deque.append(os.path.dirname(os.path.abspath(__file__)))
+    dir_deque.append(start_dir)
     while dir_deque:
         curr_dir = dir_deque.popleft()
         dir_dict[curr_dir] = analyze_dir(curr_dir)
         for dir_name in dir_dict[curr_dir].dir_path_list:
             dir_deque.append(dir_name)
+
+    for k in dir_dict:
+        print(k, dir_dict[k])
 
 do_the_thing()
