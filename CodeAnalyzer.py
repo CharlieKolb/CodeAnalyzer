dec879c3c47a67568422a86d605251f1a72354ef
Charlie Kolb <charlie.kolb@outlook.de>
2017-09-24 07:00:56 +0200

    Added commented out line, blank line and brace only line count

--- CodeAnalyzer.py
+++ CodeAnalyzer.py
@@ -10,6 +10,9 @@ ToDo Options:
 -exclude_file <regex>                                           files to be ignored
 -count_unknown                                                  also count amount and line/char count of files of unknown type (e.g. csv, png)
 -help                                                           help
+ToDo Program:
+    Use fitting comment syntax
+    Optimization: Only do extensive counting if file is in extension list
 '''
 
 
@@ -20,30 +23,36 @@ DirData = namedtuple("DictData", "amount_of_files file_name_list dir_path_list")
 # FileData = namedtuple("FileData", "extension line_count char_count")
 # LangData = namedtuple("LangData", "file_count line_count char_count")
 
+# ToDo: Generalize this for different languages?
+brace_only_options = [")", "(", "{", "}", ") {", "){"]
 
+# ToDo: Generalize these two into the same, differentiated by name in dict
 class FileData:
-    def __init__(self, extension, line_count=0, char_count=0):
+    def __init__(self, extension, line_count=0, char_count=0, commented_out_count=0, blank_count=0, brace_only_count=0):
         self.extension = extension
         self.line_count = line_count
         self.char_count = char_count
+        self.commented_out_count = commented_out_count
+        self.blank_count = blank_count
+        self.brace_only_count = brace_only_count
 
 
 class LangData:
-    def __init__(self, file_count=0, line_count=0, char_count=0):
+    def __init__(self, file_count=0, line_count=0, char_count=0, commented_out_count=0, blank_count=0, brace_only_count=0):
         self.file_count = file_count
         self.line_count = line_count
         self.char_count = char_count
-
-    def __add__(self, other):
-        return LangData(file_count=self.file_count + other.file_count,
-                        line_count=self.line_count + other.line_count,
-                        char_count=self.char_count + other.char_count)
+        self.commented_out_count = commented_out_count
+        self.blank_count = blank_count
+        self.brace_only_count = brace_only_count
 
     def __repr__(self):
         # ToDo Dynamically assign maximum required spaces for formatting
-        return "[file_count = {0:6}, line_count = {1:8}, char_count = {2:15}]"\
-            .format(self.file_count, self.line_count, self.char_count)
+        return ("[file_count = {0:6}, line_count = {1:8}, char_count = {2:15}, commented_out_count = {3:8}," \
+                + " blank_count = {4:8}, brace_only_count = {5:8}]")\
+            .format(self.file_count, self.line_count, self.char_count, self.commented_out_count, self.blank_count, self.brace_only_count)
 
+# ToDo: Read this from csv
 file_extensions = ['.java', '.js', '.c', '.h', '.cpp', '.hpp', '.cs', '.py', '.php']
 
 flag_abs_path = False
@@ -54,15 +63,37 @@ def main():
     pass
 
 
+# ToDo:
+#   - Distribution of line length?
 def parse_file(file, extension):
-    line_count, char_count = 0, 0
+    line_count, char_count, commented_out_count, blank_count, brace_only_count = 0, 0, 0, 0, 0
+    in_block_comment = False
     try:
         for line in file:
             line_count += 1
             char_count += len(line)
+            stripped = line.strip()
+            if not in_block_comment:
+                if len(stripped) == 0:
+                    blank_count += 1
+                elif len(stripped) <= 3:
+                    if stripped in brace_only_options:
+                        brace_only_count += 1
+                elif stripped.startswith("//"):
+                    commented_out_count += 1
+                elif stripped.startswith("/*"):
+                    commented_out_count += 1
+                    in_block_comment = True
+                # this checks for start of comment blocks
+                # catches a "print(\/*)", but not a "/* print(\/*", which seems like a pretty rare edge case
+                elif "/*" in stripped and not "\\/*" in stripped:
+                    in_block_comment = True
+            else:
+                if "*/" in line:
+                    in_block_comment = False
     except UnicodeDecodeError:
         pass
-    return FileData(extension, line_count, char_count)
+    return FileData(extension, line_count, char_count, commented_out_count, blank_count, brace_only_count)
 
 
 def analyze_dir(path):
@@ -85,14 +116,14 @@ def analyze_dir(path):
 def analyze_file(path):
     if not os.path.isfile(path):
         raise FileNotFoundError("File {0} not found!".format(path))
-    line_count, char_count, comment_line_count, whitespace_count = 0, 0, 0, 0
+    line_count, char_count, comment_line_count, whitespace_count, brace_only_count = 0, 0, 0, 0, 0
     char_count = 0
     _, extension = os.path.splitext(path)
     name = path if flag_abs_path else os.path.relpath(path)
     # if extension not in file_extensions:
     #    raise ExtensionError(extension)
     file = open(path, 'r')
-    print(path, extension)
+    # print(path, extension)
     return name, parse_file(file, extension)
 
 
@@ -128,11 +159,14 @@ def order_by_lang(file_dict):
     lang_dict = dict()
     for k, v in file_dict.items():
         if v.extension not in lang_dict:
-            lang_dict[v.extension] = LangData(1, v.line_count, v.char_count)
+            lang_dict[v.extension] = LangData(1, v.line_count, v.char_count, v.commented_out_count, v.blank_count, v.brace_only_count)
         else:
             lang_dict[v.extension].file_count += 1
             lang_dict[v.extension].line_count += v.line_count
             lang_dict[v.extension].char_count += v.char_count
+            lang_dict[v.extension].commented_out_count += v.commented_out_count
+            lang_dict[v.extension].blank_count += v.blank_count
+            lang_dict[v.extension].brace_only_count += v.brace_only_count
     return lang_dict
 
 do_the_thing()
