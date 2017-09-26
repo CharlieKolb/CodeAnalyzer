53c1e893cd47d58f5fbb3eb9b4c5d4899bb60a57
Charlie Kolb <charlie.kolb@outlook.de>
2017-09-26 20:16:24 +0200

    Reformatted data classes into a general LineData class Creation and maintenance of extension_dict now happens in do_the_thing()

--- CodeAnalyzer.py
+++ CodeAnalyzer.py
@@ -15,27 +15,43 @@ ToDo Program:
     Optimization: Only do extensive counting if file is in extension list
 '''
 
+flag_abs_path = False
+flag_count_unknown_extensions = False
 
-class ExtensionError(Exception):
-    pass
 
-DirData = namedtuple("DictData", "amount_of_files file_name_list dir_path_list")
-# FileData = namedtuple("FileData", "extension line_count char_count")
-# LangData = namedtuple("LangData", "file_count line_count char_count")
+DirData = namedtuple("DirData", "amount_of_files file_name_list dir_path_list")
 
 # ToDo: Generalize this for different languages?
 brace_only_options = [")", "(", "{", "}", ") {", "){", "};", ");", "{}"]
 
 
-# ToDo: Generalize these two into the same, differentiated by name in dict
-class FileData:
-    def __init__(self, extension, line_count=0, char_count=0, commented_out_count=0, blank_count=0, brace_only_count=0):
-        self.extension = extension
-        self.line_count = line_count
-        self.char_count = char_count
+class LineData:
+    def __init__(self, total_line_count=0, commented_out_count=0, blank_count=0, brace_only_count=0, char_count=0):
+        self.effective_line_count = total_line_count - commented_out_count - blank_count - brace_only_count
+        self.total_line_count = total_line_count
         self.commented_out_count = commented_out_count
         self.blank_count = blank_count
         self.brace_only_count = brace_only_count
+        self.char_count = char_count
+
+    def __add__(self, other):
+        return LineData(self.total_line_count + other.total_line_count,
+                        self.commented_out_count + other.commented_out_count,
+                        self.blank_count + other.blank_count,
+                        self.brace_only_count + other.brace_only_count,
+                        self.char_count + other.char_count)
+
+    def __repr__(self):
+        # ToDo Dynamically assign maximum required spaces for formatting
+        return ("[effective_line_count = {0:8}, total_count = {1:10}, commented_out_count = {2:8},"
+                + " blank_count = {3:8}, brace_only_count = {4:8}, char_count = {5:12}]")\
+            .format(self.effective_line_count, self.total_line_count, self.commented_out_count, self.blank_count, self.brace_only_count, self.char_count)
+
+    def as_csv_line(self):
+        return "{0},{1},{2},{3},{4},{5}"\
+            .format(self.effective_line_count, self.total_line_count, self.commented_out_count, self.blank_count, self.brace_only_count, self.char_count)
+
+    csv_header = "effective,total,commented_out,blank,braces_only,char_count"
 
 
 class LangData:
@@ -49,14 +65,12 @@ class LangData:
 
     def __repr__(self):
         # ToDo Dynamically assign maximum required spaces for formatting
-        return ("[file_count = {0:6}, line_count = {1:8}, char_count = {2:15}, commented_out_count = {3:8}," \
+        return ("[file_count = {0:6}, line_count = {1:8}, char_count = {2:15}, commented_out_count = {3:8},"
                 + " blank_count = {4:8}, brace_only_count = {5:8}]")\
             .format(self.file_count, self.line_count, self.char_count, self.commented_out_count, self.blank_count, self.brace_only_count)
 
 # ToDo: Read this from csv
 file_extensions = ['.java', '.js', '.c', '.h', '.cpp', '.hpp', '.cs', '.py', '.php']
-
-flag_abs_path = False
 
 
 # ToDo handle different input parameters and call the fitting functions
@@ -66,7 +80,7 @@ def main():
 
 # ToDo:
 #   - Distribution of line length?
-def parse_file(file, extension):
+def parse_file(file):
     line_count, char_count, commented_out_count, blank_count, brace_only_count = 0, 0, 0, 0, 0
     in_block_comment = False
     try:
@@ -87,14 +101,14 @@ def parse_file(file, extension):
                     in_block_comment = True
                 # this checks for start of comment blocks
                 # catches a "print(\/*)", but not a "/* print(\/*", which seems like a pretty rare edge case
-                elif "/*" in stripped and not "\\/*" in stripped:
+                elif "/*" in stripped and "\\/*" not in stripped:
                     in_block_comment = True
             else:
                 if "*/" in line:
                     in_block_comment = False
     except UnicodeDecodeError:
         pass
-    return FileData(extension, line_count, char_count, commented_out_count, blank_count, brace_only_count)
+    return LineData(line_count, commented_out_count, blank_count, brace_only_count, char_count)
 
 
 def analyze_dir(path):
@@ -117,20 +131,19 @@ def analyze_dir(path):
 def analyze_file(path):
     if not os.path.isfile(path):
         raise FileNotFoundError("File {0} not found!".format(path))
-    line_count, char_count, comment_line_count, whitespace_count, brace_only_count = 0, 0, 0, 0, 0
-    char_count = 0
     _, extension = os.path.splitext(path)
     name = path if flag_abs_path else os.path.relpath(path)
     # if extension not in file_extensions:
     #    raise ExtensionError(extension)
     file = open(path, 'r')
     # print(path, extension)
-    return name, parse_file(file, extension)
+    return name, extension, parse_file(file)
 
 
 def do_the_thing(start_dir=os.path.dirname(os.path.abspath(__file__))):
     dir_dict = {}
     file_dict = {}
+    ext_dict = {}
     start_dir = os.path.normpath(os.path.normcase(start_dir))
     # ToDo add option for user input path
     # deque with absolute path of the file
@@ -142,32 +155,17 @@ def do_the_thing(start_dir=os.path.dirname(os.path.abspath(__file__))):
         dir_dict[curr_dir] = analyze_dir(curr_dir)
         for dir_name in dir_dict[curr_dir].dir_path_list:
             dir_deque.append(dir_name)
-    for k, v in dir_dict.items():
-        for file_name in v.file_name_list:
-            try:
-                file_key, file_data = analyze_file(k + os.path.sep + file_name)
-                if file_data is not None:
-                    file_dict[file_key] = file_data
-            except ExtensionError:
-                # maybe remove item from file_name_list?
-                pass
-
-    for k, v in order_by_lang(file_dict).items():
+    for curr_dir, dir_data in dir_dict.items():
+        for file_name in dir_data.file_name_list:
+            file_name, file_extension, file_data = analyze_file(curr_dir + os.path.sep + file_name)
+            if file_data is not None:
+                file_dict[file_name] = file_data
+                if flag_count_unknown_extensions or file_extension in file_extensions:
+                    if file_extension not in ext_dict:
+                        ext_dict[file_extension] = file_data
+                    else:
+                        ext_dict[file_extension] += file_data
+    for k, v in ext_dict.items():
         print("{:<8}:  {}".format(k, v))
-
-
-def order_by_lang(file_dict):
-    lang_dict = dict()
-    for k, v in file_dict.items():
-        if v.extension not in lang_dict:
-            lang_dict[v.extension] = LangData(1, v.line_count, v.char_count, v.commented_out_count, v.blank_count, v.brace_only_count)
-        else:
-            lang_dict[v.extension].file_count += 1
-            lang_dict[v.extension].line_count += v.line_count
-            lang_dict[v.extension].char_count += v.char_count
-            lang_dict[v.extension].commented_out_count += v.commented_out_count
-            lang_dict[v.extension].blank_count += v.blank_count
-            lang_dict[v.extension].brace_only_count += v.brace_only_count
-    return lang_dict
 
 do_the_thing()
